bf0eba1ca247456f584fe2c318f1c0df52c4dc40
superzdd <superzdd@sina.com>
2019-08-15 13:48:31 +0800

    添加微信分享功能

--- src/components/PageLoading.vue
+++ src/components/PageLoading.vue
@@ -1,68 +1,71 @@
 <template>
-  <BasePage>
-    <div class="bg abs page" :style="{backgroundImage: finalImageSrcBg}">Loading {{loadPercent}}%</div>
-  </BasePage>
+    <BasePage>
+        <div
+            class="bg abs page"
+            :style="{backgroundImage: finalImageSrcBg}"
+        >Loading {{loadPercent}}%</div>
+    </BasePage>
 </template>
 
 <script>
-import BasePage from "./BasePage.vue";
-import imageLoad from "../util/image-load.js";
+import BasePage from './BasePage.vue';
+import imageLoad from '../util/image-load.js';
 export default {
-  name: "pageLoading",
-  components: {
-    BasePage
-  },
-  data() {
-    return {
-      sharedState: window.store.state,
-      loadPercent: 0,
-      publicPath: process.env.BASE_URL,
-      imageBgSrc: "imgs/bg-loading.jpg"
-    };
-  },
-  computed: {
-    finalImageSrcBg() {
-      return `url(${this.publicPath}${this.imageBgSrc})`;
-    }
-  },
-  created() {
-    let _imageLoad = new imageLoad();
-    _imageLoad
-      .queueImage(this.sharedState.sourceArrayPreLoad)
-      .imageLoadingProgressCallback(
-        num => {
-          this.preloadPercent = Math.floor(num);
+    name: 'pageLoading',
+    components: {
+        BasePage,
+    },
+    data() {
+        return {
+            sharedState: window.store.state,
+            loadPercent: 0,
+            publicPath: process.env.BASE_URL,
+            imageBgSrc: 'imgs/bg-loading.jpg',
+        };
+    },
+    computed: {
+        finalImageSrcBg() {
+            return `url(${this.publicPath}${this.imageBgSrc})`;
         },
-        () => {
-          this.startLoading();
-        }
-      );
-  },
-  methods: {
-    startLoading() {
-      let _imageLoad = new imageLoad();
-      _imageLoad
-        .queueImage(this.sharedState.sourceArrayLoading)
-        .imageLoadingProgressCallback(
-          num => {
-            this.loadPercent = Math.floor(num);
-          },
-          () => {
-            this.$emit("loadComplete");
-          }
-        );
-    }
-  }
+    },
+    created() {
+        let _imageLoad = new imageLoad();
+        _imageLoad
+            .queueImage(this.sharedState.sourceArrayPreLoad)
+            .imageLoadingProgressCallback(
+                num => {
+                    this.preloadPercent = Math.floor(num);
+                },
+                () => {
+                    this.startLoading();
+                }
+            );
+    },
+    methods: {
+        startLoading() {
+            let _imageLoad = new imageLoad();
+            _imageLoad
+                .queueImage(this.sharedState.sourceArrayLoading)
+                .imageLoadingProgressCallback(
+                    num => {
+                        this.loadPercent = Math.floor(num);
+                    },
+                    () => {
+                        this.$emit('loadComplete');
+                    }
+                );
+        },
+    },
 };
 </script>
 
 <style lang="scss">
-@import url("../style/common.scss");
+@import url('../style/common.scss');
 
 .bg {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-size: 100% 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-size: 100% 100%;
 }
 </style>
